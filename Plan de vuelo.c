836afa106b07cadf88174dbfb50347b32dbd46b4
luzlloveras <luzlloveras7@gmail.com>
2020-10-08 16:53:35 -0300

    Update Plan de vuelo.c

--- Plan de vuelo.c
+++ Plan de vuelo.c
@@ -12,26 +12,26 @@ int main() {
     int x1, y1, x2, y2, z;
     float suma_dist=0;
 
-    printf("Ingrese coordenada x:");
+    printf("Ingrese coordenada x:\n");
     scanf("%d", &x1);
-    printf("Ingrese coordenada y:");
+    printf("Ingrese coordenada y:\n");
     scanf("%d", &y1);
-    printf("Ingrese coordenada x:");
+    printf("Ingrese coordenada x:\n");
     scanf("%d", &z);
     while (z >= 0) {
         x2=z;
-        printf("Ingrese coordenada y:");
+        printf("Ingrese coordenada y:\n");
         scanf("%d", &y2);
         suma_dist = suma_dist + distancia(x1, y1, x2, y2);
 
         x1=x2; //desplazo x2 a x1
         y1=y2; //desplazo y2 a y1
 
-        printf("\nIngrese coordenada x:");
+        printf("Ingrese coordenada x:\n");
         scanf("%d", &z);
     }
 
-    printf("\n La distancia recorrida es: %.2f", suma_dist);
+    printf("La distancia recorrida es: %.2f\n", suma_dist);
 
 
     return 0;
